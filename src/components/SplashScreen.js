08a7810eec0a85bd50d7a8ff6bee49d66738bc69
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-12-22 17:17:05 +0200

    Style splash screen

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import "./splashScreen.css";
+import UniversityLogoSVG from "../icons/UniversityLogoSVG.js";
+import UniNavLogoSVG from "../icons/UniNavLogoSVG.js";
 
 export const SplashScreen = (props) => {
-  return <div className="splash-screen"></div>;
+  return (
+    <div className="splash-screen">
+      <div className="university-logo">
+        <UniversityLogoSVG></UniversityLogoSVG>
+      </div>
+      <div className="uninav-logo">
+        <UniNavLogoSVG></UniNavLogoSVG>
+      </div>
+    </div>
+  );
 };
