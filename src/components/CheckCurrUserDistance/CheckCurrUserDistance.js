85645b04d6731593de56b1a7bf8003deb31bdd10
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-01-10 19:23:22 +0200

    Edit comments & props

--- src/components/CheckCurrUserDistance/CheckCurrUserDistance.js
+++ src/components/CheckCurrUserDistance/CheckCurrUserDistance.js
@@ -5,50 +5,46 @@ import { mapCenter } from "../../constants/mapCenter";
 import L from "leaflet";
 import getString from "../../strings/strings";
 
-export const CheckCurrUserDistance = ({
-  isFirstRender,
-  currLocationOptions,
-  setIsChangeMapView,
-  setIsFirstRender,
-  setIsCenterUserLocation,
-}) => {
+export const CheckCurrUserDistance = (props) => {
   const map = useMap();
 
+  // Check the user distance from the uni, at the app's first render
   useEffect(() => {
     let timeOut;
-    if (isFirstRender) {
+
+    // Wait a few seconds after the first render & center the map according to the user location
+    if (props.isFirstRender) {
       timeOut = setTimeout(() => {
-        setIsChangeMapView(true);
-        setIsFirstRender(false);
+        props.setIsChangeMapView(true);
+        props.setIsFirstRender(false);
 
         measureCurrUserDistance();
       }, 8000);
     }
     return () => clearTimeout(timeOut);
-  }, [isFirstRender]);
+  }, [props.isFirstRender]);
 
   function measureCurrUserDistance() {
+    // Init coords of the university center
     const centerLatLng = L.latLng(mapCenter.lat, mapCenter.lng);
+
+    // Init coords of the current user position
     const currPositionLatLng = L.latLng(
-      currLocationOptions.location.latitude,
-      currLocationOptions.location.longitude
+      props.currLocationOptions.location.latitude,
+      props.currLocationOptions.location.longitude
     );
+
+    // Check the distance between the university center & the current user position
     const distance = map.distance(centerLatLng, currPositionLatLng);
 
-    console.log(
-      "centerLatLng ",
-      centerLatLng,
-      "currPositionLatLng ",
-      currPositionLatLng,
-      "distance ",
-      distance
-    );
-
+    // Check the distance of the user from the university
     if (distance > 1000) {
+      // If the user isn't at the universy, center the map to the user location only if he confirmed
       const isFollowDistancedUser = window.confirm(getString("TOO_FAR"));
-      setIsCenterUserLocation(isFollowDistancedUser);
+      props.setIsCenterUserLocation(isFollowDistancedUser);
     } else {
-      setIsCenterUserLocation(true);
+      // center the map to the user location
+      props.setIsCenterUserLocation(true);
     }
   }
 
