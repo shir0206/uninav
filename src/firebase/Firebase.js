02070b6c9120e7a043b10a41dcbd4d0ab171de0e
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-01-15 12:46:07 +0200

    Test .env.local

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -11,6 +11,19 @@ const firebaseConfig = {
   measurementId: "G-C7TR96X7EH",
 };
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASURE_ID,
+// };
+
+//= process.env.FIREBASE_CONFIG;
+
 // Initialize Firebase
 try {
   firebase.initializeApp(firebaseConfig);
@@ -22,7 +35,6 @@ try {
   }
 }
 // Get a reference to the database service
-var database = firebase.database();
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
 export default firebase;
