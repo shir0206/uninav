96bbbc3b664914d4127a14f1d0419a076802242d
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-01-21 19:48:24 +0200

    Remove comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import "./styles/colors.css";
 import { SplashScreen } from "./components/SplashScreen/SplashScreen";
-import { LocateUserButton } from "./components/LocateUserButton/LocateUserButton";
 import { Map } from "./components/Map/Map";
 import NavbarRouter from "./components/NavbarRouter/NavbarRouter";
 
 function App() {
-  // const [isLocateUser, setIsLocateUser] = useState(true);
-  // const [isCenterUserLocation, setIsCenterUserLocation] = useState(false);
-  // const [isLocationError, setIsLocationError] = useState(false);
   const [displayPOITypes, setDisplayPOITypes] = useState({
     bench: false,
     area: true,
