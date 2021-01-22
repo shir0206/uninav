10fba273d275e7d06cbe23e67e28d494cd07e376
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-01-22 15:54:11 +0200

    Fix algorithm for identifying the nearest poi

--- src/components/CheckCurrUserDistanceFromPOI/CheckCurrUserDistanceFromPOI.js
+++ src/components/CheckCurrUserDistanceFromPOI/CheckCurrUserDistanceFromPOI.js
@@ -8,34 +8,38 @@ export const CheckCurrUserDistanceFromPOI = (props) => {
 
   const map = useMap();
 
-  function measureCurrUserDistanceFromPOI(poi, userLocation) {
-    // Init coords of the university center
-    const poiLatLng = L.latLng(poi.location.lat, poi.location.lng);
-
-    // Init coords of the current user position
-    const currPositionLatLng = L.latLng(
-      userLocation.latitude,
-      userLocation.longitude
-    );
-
-    // Check the distance between the university center & the current user position
-    const distance = map.distance(poiLatLng, currPositionLatLng);
-
-    // Check the distance of the user from the university
-    if (distance < 1.5) {
-      // If the user isn't at the universy, center the map to the user location only if he confirmed
-      window.alert("arrived" + poi.id);
-      setNearPOI(poi);
-    } else {
-      setNearPOI(null);
-    }
-  }
-
   useEffect(() => {
     if (props.mapPOIs && props.currLocationOptions.location) {
-      props.mapPOIs.map((poi) => {
-        measureCurrUserDistanceFromPOI(poi, props.currLocationOptions.location);
-      });
+      for (let i = 0; i < props.mapPOIs.length; i++) {
+        let poi = props.mapPOIs[i];
+        let userLocation = props.currLocationOptions.location;
+
+        // Init coords of the curr poi
+        const poiLatLng = L.latLng(poi.location.lat, poi.location.lng);
+
+        // Init coords of the current user position
+        const currPositionLatLng = L.latLng(
+          userLocation.latitude,
+          userLocation.longitude
+        );
+
+        // Check the distance between the curr poi & the current user position
+        const distance = map.distance(poiLatLng, currPositionLatLng);
+
+        // Check the distance of the user from the university
+        if (distance < 1.5) {
+          // Set the curr poi as nearest (if hasn't set yet)
+          if (nearPOI !== poi) {
+            setNearPOI(poi);
+            console.log("arrived " + poi.id);
+            window.alert("arrived " + poi.id);
+          }
+          return;
+        }
+      }
+
+      setNearPOI(null);
+      console.log("not arrived");
     }
   }, [props.currLocationOptions.location, props.mapPOIs]);
 
