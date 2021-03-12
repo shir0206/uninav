eec5654fdb8c1f2b1a09bd88e00986ca63ba87b5
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-03-12 18:10:44 +0200

    Fix vh units on mobile

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return (
     <>
       {loading && <SplashScreen />}
